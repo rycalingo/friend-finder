5c10512f9440231610eb625e6b5b028769cf6686
Ryan Calingo <gryan.calingo@gmail.com>
2018-02-12 02:50:35 -0500

    add server scripts

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 // =============================
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
