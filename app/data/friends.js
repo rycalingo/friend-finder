3165183b4e4cedde08a6b1eafa3c47ae1308af3f
Ryan Calingo <gryan.calingo@gmail.com>
2018-02-12 02:50:52 -0500

    add friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,56 @@
-// friends.js
+let friendsArray = [
+    {
+      name: "Jon Snow",
+      photo: "jon_photo.jpg",
+      scores: [1,5,2,3,4,5,5,1,4,3]
+    },
+    {
+      name: "Cersei Lannister",
+      photo: "cersei_photo.jpg",
+      scores: [5,4,5,5,1,5,5,1,1,1]
+    },
+    {
+      name: "Daenerys Targaryen",
+      photo: "daenerys_photo.jpg",
+      scores: [5,4,4,1,3,3,1,2,4,5]
+    },
+    {
+      name: "Tyrion Lannister",
+      photo: "tyrion_photo.jpg",
+      scores: [5,3,5,2,5,1,1,3,4,5]
+    },
+    {
+      name: "Jaime Lannister",
+      photo: "jaime_photo.jpg",
+      scores: [1,4,3,2,4,2,2,5,4,5]
+    },
+    {
+      name: "Brienne Tarth",
+      photo: "brienne_photo.jpg",
+      scores: [2,2,3,1,1,3,5,1,1,1]
+    },
+    {
+      name: "Grey Worm",
+      photo: "grey_photo.jpg",
+      scores: [1,2,4,1,3,5,5,1,2,1]
+    },
+    {
+      name: "Melisandre",
+      photo: "melisandre_photo.jpg",
+      scores: [4,5,2,3,4,1,1,5,4,5]
+    },
+    {
+      name: "Sansa Stark",
+      photo: "sansa_photo.jpg",
+      scores: [5,2,4,5,2,4,4,5,1,1]
+    },
+    {
+      name: "Jorah Mormont",
+      photo: "jorah_photo.jpg",
+      scores: [3,2,2,1,1,5,5,2,1,1]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
