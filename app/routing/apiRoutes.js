b788558733950dd87bfa17cff81ef6eff809ead4
Ryan Calingo <gryan.calingo@gmail.com>
2018-02-16 01:37:59 -0500

    match function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,22 @@
 
 let friendsData = require('../data/friends');
 
+const matchFriend = function(friendsList, newPerson) {
+  let prev = 40;
+  let best_match;
 
+  for (const [i, friend] of friendsList.entries()) {
+   
+    let score = friend.scores.reduce( (sum, friend_score) => sum + Math.abs(friend_score - newPerson.scores[i]) );
+    console.log(score);
+    if ( score <= prev ) {
+      prev = score;
+      best_match = friend;
+    }
+  };
+
+  return best_match;
+}
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
@@ -9,8 +24,20 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    friendsData.push(req.body);
-    res.json(req.body);
+    let newFriend = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: req.body["scores[]"]
+    }
+
+    match = matchFriend(friendsData, newFriend);
+
+    console.log(match);
+    // friendsData.push(req.body);
+
+    res.json(match);
   });
+
+
 
 };
